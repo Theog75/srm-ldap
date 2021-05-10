692dbf11f7c237e569c9653cae3f30012e05c1c3
root <root@SR-Utils.lhu1ca5o0xpe3g2aykmzrsmi2b.bx.internal.cloudapp.net>
2021-05-10 14:58:08 +0000

    i

---
+++ env.sh
@@ -0,0 +1,7 @@
+export LDAPHOST=52.255.178.11
+export LDAPBINDPASSWORD=Aa123456
+export LDAPBASEDN=CN=Users,DC=test,DC=local
+export LDAPPORT=389
+export LDAPSSL=false
+export LDAPBINDDN="CN=bind ldap,CN=Users,DC=test,DC=local"
+export PORT="8086"
