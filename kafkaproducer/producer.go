28070ba153120903506dde60f5706b8f8d1e2169
momoadc <momoadcch@gmail.com>
2021-05-18 17:20:29 +0300

    jasonify

--- kafkaproducer/producer.go
+++ kafkaproducer/producer.go
@@ -2,6 +2,7 @@ package kafkaproducer
 
 import (
 	"context"
+	"encoding/json"
 	"time"
 
 	"fmt"
@@ -11,6 +12,11 @@ import (
 	kafka "github.com/segmentio/kafka-go"
 )
 
+type MessageFormat struct {
+	Timestamp time.Time
+	Data      string
+}
+
 func ProducerHandler(ProduceMsg string) {
 	// get kafka writer using environment variables.
 	kafkaURL := strings.Split(config.KafkaURL, ",")
@@ -18,9 +24,20 @@ func ProducerHandler(ProduceMsg string) {
 	writer := newKafkaWriter(kafkaURL, topic)
 	defer writer.Close()
 	fmt.Println("start producing ... !!")
+	baseMessage := MessageFormat{
+		Timestamp: time.Now(),
+		Data:      ProduceMsg,
+	}
+
+	jsonMessage, err := json.Marshal(baseMessage)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	msg := kafka.Message{
 		Key:   []byte("collector"),
-		Value: []byte(ProduceMsg),
+		Value: []byte(jsonMessage),
 	}
 	writer.WriteMessages(context.Background(), msg)
 	time.Sleep(1 * time.Second)
