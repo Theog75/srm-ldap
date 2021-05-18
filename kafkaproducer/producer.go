cf7e1b38365552f2aea0cc93f77b7fc37b023dfb
momoadc <momoadcch@gmail.com>
2021-05-18 13:26:56 +0300

    d

--- kafkaproducer/producer.go
+++ kafkaproducer/producer.go
@@ -4,30 +4,29 @@ import (
 	"context"
 	"time"
 
-	kafka "github.com/segmentio/kafka-go"
-
 	"fmt"
 	"srm-ldap/config"
+	"strings"
+
+	kafka "github.com/segmentio/kafka-go"
 )
 
 func ProducerHandler(ProduceMsg string) {
 	// get kafka writer using environment variables.
-	kafkaURL := config.KafkaURL
+	kafkaURL := strings.Split(config.KafkaURL, ",")
 	topic := config.KafkaTopic
 	writer := newKafkaWriter(kafkaURL, topic)
-	fmt.Println("msg:\n", ProduceMsg)
 	defer writer.Close()
 	fmt.Println("start producing ... !!")
 	msg := kafka.Message{
 		Key:   []byte("collector"),
 		Value: []byte(ProduceMsg),
 	}
-	fmt.Println(msg)
 	writer.WriteMessages(context.Background(), msg)
 	time.Sleep(1 * time.Second)
 }
 
-func newKafkaWriter(kafkaURL, topic string) *kafka.Writer {
+func newKafkaWriter(kafkaURL []string, topic string) *kafka.Writer {
 	return &kafka.Writer{
 		Addr:     kafka.TCP(kafkaURL),
 		Topic:    config.KafkaTopic,
